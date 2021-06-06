20dae17e83a59ba822cb00d1d27ddf0e02f84fe4
Nikhil Lele <nik.lele@gmail.com>
2021-06-06 11:36:45 -0700

    open whitenoise site in browser

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 1. Build whitenoise.exe: `make`
 2. Get the executable: `explorer.exe .` and then copy whitenoise.exe
 
+## Run
+
+Run whitenoise.exe
+
+## References
+- Volume control: https://github.com/itchyny/volume-go
+- Opening browser: https://gist.github.com/hyg/9c4afcd91fe24316cbf0
--- main.go
+++ main.go
@@ -1,27 +1,30 @@
+// +build windows
+
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
+	"os/exec"
 
 	"github.com/itchyny/volume-go"
 )
 
 func main() {
 	volume := 30
+	url := "https://mynoise.net/NoiseMachines/whiteNoiseGenerator.php?l=14222840464228211305&m=&d=0"
+
 	log.Println("start whitenoise")
 	setVolumeGoal(volume)
+	openBrowser(url)
 
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		_, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatalf("get input failed: %+v", err)
-		}
-	}
+	// infinite read input loop
+	// reader := bufio.NewReader(os.Stdin)
+	// for {
+	// 	_, err := reader.ReadString('\n')
+	// 	if err != nil {
+	// 		log.Fatalf("get input failed: %+v", err)
+	// 	}
+	// }
 }
 
 func setVolumeGoal(vol int) {
@@ -29,11 +32,18 @@ func setVolumeGoal(vol int) {
 	if err != nil {
 		log.Fatalf("set volume failed: %+v", err)
 	}
-	fmt.Printf("set volume to: %d\n", vol)
+	log.Printf("set volume to: %d\n", vol)
 
 	readVol, err := volume.GetVolume()
 	if err != nil {
 		log.Fatalf("get volume failed: %+v", err)
 	}
-	fmt.Printf("current volume: %d\n", readVol)
+	log.Printf("current volume: %d\n", readVol)
+}
+
+func openBrowser(url string) {
+	cmd := exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("open browser failed: %+v", err)
+	}
 }
